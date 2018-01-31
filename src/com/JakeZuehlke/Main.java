cda48274c9c2cd1155a5d20d388a7298a7dada8d
L115 Student <L115student@bitnet.wctc.edu>
2018-01-31 12:13:51 -0600

    Initial commit

---
+++ src/com/JakeZuehlke/Main.java
@@ -0,0 +1,9 @@
+package com.JakeZuehlke;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/com/JakeZuehlke/TestScores.java
@@ -0,0 +1,15 @@
+package com.JakeZuehlke;
+
+/**
+ * @author Jake Zuehlke
+ * TestScores class:
+ * - accepts and array of test scores as its argument
+ * - can return average of test scores given
+ * - throws IllegalArgumentException for scores less than 0 or greater than 100
+ */
+public class TestScores
+{
+    private int[] scores;
+
+    
+}
